40fd4da180272af830e6a1856865212ab9d68eb4
manenti jérérmi <jeremi.manenti@etu.u-bordeaux1.fr>
2013-01-24 18:12:46 +0100

    commence scroll

--- Sandbox/src/com/example/sandbox/BaseDEDonnee.java
+++ Sandbox/src/com/example/sandbox/BaseDEDonnee.java
@@ -14,15 +14,23 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.ScrollView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.support.v4.app.NavUtils;
 
 public class BaseDEDonnee extends Activity {
-	
+	public final static String EXTRA_MESSAGE = "null";
+	public String parent = "null";
 	private Spinner spinner2;
 	private Button btnSubmit;
+	private RadioGroup radioView;
+	private RadioButton radioButton;
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,6 +39,17 @@ public class BaseDEDonnee extends Activity {
         
         addItemsOnSpinner2();
         addListenerOnButton();
+        
+        /*****************scollll*******************************/
+        radioView = (RadioGroup) this.findViewById(R.id.radioAterissage);
+        
+        for(int i=0;i<=10;i++){
+        	radioButton = new RadioButton(this);
+        	radioButton = (RadioButton) findViewById(R.id.radioButton);
+        	radioButton.setText("sadfasdfasdfasdfasdfasd fasdfsadfsadf");
+        }
+        
+        
     }
     
     public void addItemsOnSpinner2() {
@@ -40,6 +59,7 @@ public class BaseDEDonnee extends Activity {
     	list.add("list 1");
     	list.add("list 2");
     	list.add("list 3");
+
     	ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list);
     	dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
     	spinner2.setAdapter(dataAdapter);
@@ -75,6 +95,13 @@ public class BaseDEDonnee extends Activity {
     
     public void goToAccueil(View view) {
     	Intent intent = new Intent(this, Accueil.class);
+    	startActivity(intent);
+    }
+    
+    public void retourParent(View view) {
+    	Intent intent = new Intent(this, MainActivity.class);
+    	String message = parent = String.valueOf(spinner2.getSelectedItem());
+    	intent.putExtra(EXTRA_MESSAGE, message);
     	startActivity(intent);
     }
     
--- Sandbox/src/com/example/sandbox/MainActivity.java
+++ Sandbox/src/com/example/sandbox/MainActivity.java
@@ -1,4 +1,5 @@
 package com.example.sandbox;
+
 import Utils.*;
 
 import com.example.sqllite.Livre;
@@ -10,17 +11,29 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
     public static String ISBN = "Nom ";
     public static String TITLE = "Description";
     public static String PARENT = "parent";
+    public static String parent = "Entre un parent";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        
+        EditText paramParent = null;
+        paramParent = (EditText)findViewById(R.id.parent_edit_message);
+        
+        Intent my_intent = getIntent();
+        parent = my_intent.getStringExtra(BaseDEDonnee.EXTRA_MESSAGE);
+        Toast.makeText(MainActivity.this, parent, Toast.LENGTH_SHORT).show();
+        if(parent != null && parent != "Entre en parent")
+        	paramParent.setHint(parent);
+        
+        
         try {
         	LivresBDD lbdd = new LivresBDD(this);
         	lbdd.open();
@@ -47,7 +60,6 @@ public class MainActivity extends Activity {
     	EditText isbn = (EditText) findViewById(R.id.isbn_edit_message);
     	EditText title = (EditText) findViewById(R.id.title_edit_message);
     	EditText parent = (EditText) findViewById(R.id.parent_edit_message);
-    	
     	if (!isbn.getText().toString().equals(""))
     	{
     		ISBN = isbn.getText().toString();
